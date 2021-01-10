2c5963627b5763950822d63efd54eafc6bdd308e
JMax45 <maxmailru.45@gmail.com>
2021-01-10 18:00:47 +0100

    fix: switch blocks position and change header

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,8 +1,8 @@
 const content = [
-    { body: "Testo 1 | Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure magni soluta facilis quasi voluptates voluptate id tempora consectetur nisi perferendis!" },
-    { body: "Testo 2 | Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure magni soluta facilis quasi voluptates voluptate id tempora consectetur nisi perferendis!" },
-    { body: "Testo 3 | Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure magni soluta facilis quasi voluptates voluptate id tempora consectetur nisi perferendis!" },
-    { body: "Testo 4 | Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure magni soluta facilis quasi voluptates voluptate id tempora consectetur nisi perferendis!" }
+    { body: "Stanco di rimandare viaggi perché hai problemi con il tuo animale domestico? Contattami!" },
+    { body: "Sapevi che il muso di ogni cane è unico? Non possono esistere al mondo due musi uguali, proprio come succede con le impronte digitali dell'uomo. Anche i cani hanno i propri tratti distintivi." },
+    { body: "Il colore del tartufo potrebbe cambiare a causa di una bruciatura o per conto del cambio climatico." },
+    { body: "I baffi dei cani, per quanto molto più corti dei corrispettivi felini, hanno la particolare funzione di percepire il benché minimo spostamento d'aria e avvisare previamente su forma, dimensione e distanza da un eventuale pericolo. Sono utilissimi specialmente di notte, quando la visione è molto ridotta." }
 ]
 
 const slideshow = document.getElementsByClassName('interesting-facts')[0];
